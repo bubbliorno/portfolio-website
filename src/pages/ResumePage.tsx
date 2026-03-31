a1d262f42858bf0ef2412e3d57e25cc2d90fc782
bubbliorno <dasparkleguy@gmail.com>
2026-03-31 14:26:55 -0400

    updated link to resume

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -5,10 +5,10 @@ export default function ResumePage() {
     <section className="py-8">
       <h2 className="text-2xl font-medium">Resume</h2>
       <a
-        href="/public/Anthony_Vo_Resume.pdf"
+        href="/Anthony_Vo_Resume.pdf" // Append /public/ to the front of the link to test in development
         target="_blank"
         rel="noopener noreferrer"
-        className="mt-6 inline-flex items-center gap-2 rounded-lg bg-text px-6 py-3 font-medium text-background transition-opacity hover:opacity-90"
+        className="bg-text text-background mt-6 inline-flex items-center gap-2 rounded-lg px-6 py-3 font-medium transition-opacity hover:opacity-90"
       >
         <FaFilePdf size={20} />
         View Resume
